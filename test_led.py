dcaec8684613eb0280d4ca1cddeddcf6cacb034a
Eya Chafter <eya.chafter@medtech.tn>
2021-02-19 16:23:19 +0100

    first test

---
+++ test_led.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(8, GPIO.OUT , initial=GPIO.LOW)
+
+while True:
+    GPIO.output(8,GPIO.HIGH)
+    sleep(2)
+    GPIO.output(8,GPIO.LOW)
+    sleep(2)
+
